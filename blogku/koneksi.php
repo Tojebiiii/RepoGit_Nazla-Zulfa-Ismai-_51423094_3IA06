18370c76e57208e89f8cb948be1025a5271bdf23
Nazla <nazlaazulfa@gmail.com>
2025-12-30 15:00:48 +0700

    koneksi.php

--- blogku/koneksi.php
+++ blogku/koneksi.php
@@ -1,15 +1,16 @@
 <?php
-// Pengaturan Koneksi Database
+
 $host = "localhost";
-$user = "root";     // Default XAMPP
-$pass = "";         // Default XAMPP (kosong)
+$user = "root";    
+$pass = "";         
 $db_name = "diary_blog";
 
-// Membuat koneksi
+
 $koneksi = mysqli_connect($host, $user, $pass, $db_name);
 
-// Mengecek koneksi
+
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
 ?>
